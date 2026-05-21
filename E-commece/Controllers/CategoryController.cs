534b612ee10dad3fa1ed4bada26162f2199b35c5
Mohamed <mohamedwwe1111@gmail.com>
2026-05-21 22:08:14 +0300

    Get all Categories

---
+++ E-commece/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using E_commece.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commece.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
