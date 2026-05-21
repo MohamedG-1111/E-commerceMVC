cc9e4729d106a932ddb481c1f7f868c38333f8cc
Mohamed <mohamedwwe1111@gmail.com>
2026-05-21 19:22:48 +0300

    SeedDateToCategoryTable

--- E-commece/Data/AppDbContext.cs
+++ E-commece/Data/AppDbContext.cs
@@ -8,6 +8,14 @@ namespace E_commece.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Electronics", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Clothes", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Food", DisplayOrder = 3 }
+            );
+        }
         public DbSet<Category> Categories { get; set; }
     }
 }
