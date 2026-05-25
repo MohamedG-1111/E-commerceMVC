407b5cf3e009659576f0245f091eda1879504cf6
Mohamed <mohamedwwe1111@gmail.com>
2026-05-25 19:13:02 +0300

    Refactor Category module to use Service Layer
    * Introduced ICategoryService
    * Moved CRUD logic from Controller to Service
    * Implemented Unit of Work pattern
    * Added ViewModel mapping
    * Improved separation of concerns

---
+++ BLL/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using BLL.ViewModels;
+using DataAcessLayer.Models;
+
+namespace BLL.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<bool> CreateCategoryAsync(CategoryVM obj);
+        public Task<CategoryVM?> CategoryDetailsAsync(int Id);
+
+        public Task<IEnumerable<Category>?> AllCategoriesAsync();
+
+        public Task<bool> UpdateCategoryAsync(int id, CategoryVM Obj);
+        public Task<bool> DeleteCategoryAsync(int id);
+
+    }
+}
