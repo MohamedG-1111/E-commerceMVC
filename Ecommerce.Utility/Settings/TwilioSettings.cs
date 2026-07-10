182666de49b6a21a1a5e39b2f903c5472d59d413
Mohamed <mohamedwwe1111@gmail.com>
2026-07-10 15:31:40 +0300

    Add Twilio settings support and remove Stripe config
    Added TwilioSettings class and registered it in DI container. Removed StripeSettings section from appsettings.json.

---
+++ Ecommerce.Utility/Settings/TwilioSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce.Utility.Settings
+{
+    public class TwilioSettings
+    {
+        public string AccountSID { get; set; } = null!;
+
+        public string AuthToken { get; set; } = null!;
+
+        public string TwilioPhoneNumber { get; set; } = null!;
+    }
+}
